089c2286982fac67ab810104ed00274b5b32aef6
Keren Villatoro <kerenv@outlook.com>
2024-05-10 17:33:52 -0500

    db setup

---
+++ src/app/api/words/route.ts
@@ -0,0 +1,18 @@
+//https://datalab.medium.com/nextjs-14-json-api-with-mysql-9f635b5ecb1d
+import { NextResponse } from "next/server";
+import pool from "@/app/lib/mysql";
+
+export async function GET() {
+    try {
+        const db = await pool.getConnection();
+        const query = "SELECT * FROM words ORDER BY RAND() LIMIT 20";
+        const [rows] = await db.execute(query)
+        db.release()
+
+        return NextResponse.json(rows)
+    } catch (error) {
+        return NextResponse.json({
+            error: error
+        }, { status: 500 })
+    }
+}
