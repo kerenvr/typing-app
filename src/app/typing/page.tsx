d8006da725089d98fd921411ee3047d069d6458e
Keren Villatoro <kerenv@outlook.com>
2024-05-14 11:46:41 -0500

    Rearranged code and set current char and index

--- src/app/typing/page.tsx
+++ src/app/typing/page.tsx
@@ -1,56 +1,67 @@
 'use client'
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { fetchWords } from '@/utils/fetchWords';
 
 function Typing() {
-  const [words, setWords] = useState([]);
   const [keyPressed, setKeyPressed] = useState('');
-  const [wordArr, setWordArr] = useState<string[]>([]);
-
+  const [words, setWords] = useState<string>('');
+  const [currChar, setCurrChar] = useState<string>('');
   const [index, setIndex] = useState(0);
+  const [color, setColor] = useState<string>('');
+
+  const updateIndexandCurrChar = (change: number) => {
+    setIndex(prevIndex => {
+      const newIndex = prevIndex + change;
+      setCurrChar(words[newIndex]);
+      return newIndex
+    });
+  };
 
   useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key.length === 1) { //only a letter or punctuation 
-        setKeyPressed(prevKeyPressed => prevKeyPressed + e.key); //append all letters typed
-      } else if (e.key === 'Backspace') { //delete
-        setKeyPressed(prevKeyPressed => prevKeyPressed.slice(0, -1));
-      }
-    }
+    const getWords = async () => {
+      const { tempArray } = await fetchWords();
+      const joinedWords = tempArray.join(' ');
+      setWords(joinedWords);
+      setCurrChar(joinedWords[0]);
+    };
 
-    window.addEventListener('keydown', handleKeyDown); //keydown listener on window
-    
-    const fetchWords = async () => {
-      const response = await fetch ('/api/words');
-      const data = await response.json();
+    getWords();
 
-      let tempArray = [];
-      for (const wordObj of data) { //there are 20 objects, it will iterate through each one and access the word only
-        const currWord = wordObj.words; //access the word
-        tempArray.push(currWord); // push onto temporary array
-      }
-      setWordArr(tempArray); //set the wordArr to the temporary array
-
-      setWords(data);
-    }
-
-    fetchWords();
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    }
-    
 }, []);
+
+useEffect(() => {
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key.length === 1) { //only a letter or punctuation 
+      setKeyPressed(prevKeyPressed => prevKeyPressed + e.key); //append all letters typed
+      if (e.key === currChar) {
+        updateIndexandCurrChar(1);
+        setColor('green');
+      } else {
+        setColor('red');
+      }
+    } else if (e.key === 'Backspace') { //delete
+      setKeyPressed(prevKeyPressed => prevKeyPressed.slice(0, -1));
+      //updateIndexandCurrChar(-1);
+    }
+  }
+
+  window.addEventListener('keydown', handleKeyDown); //keydown listener on window
+  
+
+  return () => {
+    window.removeEventListener('keydown', handleKeyDown);
+  }
+})
+
 
   return (
     <>
       <div>Typing</div>
       <div>
-         <p>Key pressed: {keyPressed}</p>
-         {words.map((wordObj, index) => (
-          <p key={index}>{wordObj.words}</p>
-         ))}
-         
+         <p style={{color: color}}>Key pressed: {keyPressed}</p>
+         <p>Words: {words}</p>
+         <p>curr: {currChar}</p>
       </div>
     </>
   )
