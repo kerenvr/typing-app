e6124687371cd1b16633c819922866df591af3f7
Keren Villatoro <kerenv@outlook.com>
2024-06-01 17:10:29 -0500

    new: sqlite and prisma setup

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,14 @@
 "use server"
 
+// import prisma from "@/lib/db"
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail, getUserByUsername } from "../data/user";
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export const Register = async  (values: z.infer<typeof RegisterSchema>) => {
     const result = RegisterSchema.safeParse(values);
@@ -9,6 +16,29 @@ export const Register = async  (values: z.infer<typeof RegisterSchema>) => {
     if (!result.success) {
         return { error: result.error.message };
     }
+
+    const { email, username, password } = values;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingEmail = await getUserByEmail(email);
+
+    const existingUser = await getUserByUsername(username);
+
+    if (existingEmail) {
+        return {error: "Email already in use!"}
+    } 
+
+    if (existingUser) {
+        return {error: "That username is taken!"}
+    } 
+    
+    await prisma.user.create({
+        data: {
+            email,
+            username,
+            password: hashedPassword,
+        },
+    })
 
     return { success: "Success!" };
 }
---
+++ src/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from "./lib/db";
+ 
+ 
+export const {
+  auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter({ prisma: prisma }),
+  session: { strategy: "jwt" },
+  ...authConfig,
+})
