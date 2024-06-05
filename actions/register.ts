5f48b72af1dc365aa07dc5278e738640575abd0d
Keren Villatoro <kerenv@outlook.com>
2024-06-05 13:37:30 -0500

    registration works

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { findUserByEmail } from "../data/user";
+import { prisma } from '@/lib/db';
 
 export const Register = async  (values: z.infer<typeof RegisterSchema>) => {
     const result = RegisterSchema.safeParse(values);
@@ -9,6 +12,29 @@ export const Register = async  (values: z.infer<typeof RegisterSchema>) => {
     if (!result.success) {
         return { error: result.error.message };
     }
+
+    const { name, email, password } = values;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingEmail = await findUserByEmail(email);
+
+    if (existingEmail) {
+        return {error: "Email already in use!"}
+    } 
+
+
+    try {
+        await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            }
+        })
+    } catch (error) {
+        return { error: 'Error creating user.' };
+    }
+
 
     return { success: "Success!" };
 }
