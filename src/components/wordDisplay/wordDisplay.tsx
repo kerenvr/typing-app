ceb8f1f3dd84ae6f2415b556e2cfdbc03c51fe57
Keren Villatoro <kerenv@outlook.com>
2024-06-12 22:13:35 -0500

    add: flex wrap words layout

--- src/components/wordDisplay/wordDisplay.tsx
+++ src/components/wordDisplay/wordDisplay.tsx
@@ -16,7 +16,6 @@ const WordDisplay: React.FC<WordDisplayProps> = ({ words, charsTyped, wpm }) => 
     const [marginTop, setMarginTop] = useState<number>(0);
     const [fontSize, setFontSize] = useState<number>(0);
     const THRESHOLD = 1.5;
-    const [value, setValue] = useState<number>(0);
 
     useEffect(() => {
         if (pRef.current) {
@@ -33,64 +32,28 @@ const WordDisplay: React.FC<WordDisplayProps> = ({ words, charsTyped, wpm }) => 
     }, [charsTyped]);
 
     useEffect(() => {
-        
-        const targetDelta = 3 * fontSize;
-        const deltaY = prevY - y;
-        setValue(prevValue => prevValue + 1);
-
-        console.log("y: ", y, "prevY: ", prevY, y-prevY, marginTop, value)
-
-        if (deltaY === targetDelta) {
-            console.log("increased!" )
-            marginTopRef.current += ((fontSize * THRESHOLD)); // Decrease margin top by targetDelta
+        if ((prevY - y) === (fontSize * 3)) {
+            marginTopRef.current = marginTopRef.current + (fontSize * THRESHOLD);
             setMarginTop(marginTopRef.current);
-            return
+            return;
+        }
+        
+        if (y === prevY) {
+            marginTopRef.current = marginTopRef.current - (fontSize * THRESHOLD);
+            setMarginTop(marginTopRef.current);
         }
 
-        // if ((prevY - y) === (fontSize * 3)) {
-        //     marginTopRef.current = marginTopRef.current + (fontSize * THRESHOLD);
-        //     setMarginTop(marginTopRef.current);
-        //     return;
-        // }
-        
-        // if (y === prevY) {
-        //     marginTopRef.current = marginTopRef.current - (fontSize * THRESHOLD);
-        //     setMarginTop(marginTopRef.current);
-        // }
-
-        if ((prevY !== y && prevY !== 0 && prevY < y) || y === prevY) {
-            console.log("decreased!")
-            marginTopRef.current -= ((fontSize * THRESHOLD));
+        if (prevY !== y && prevY !== 0 && prevY < y) {
+            marginTopRef.current = marginTopRef.current - (fontSize * THRESHOLD);
             setMarginTop(marginTopRef.current);
         }
         setPrevY(y);
-
     }, [y]);
 
     return (
         <div className={styles.container}>
-            <p ref={pRef} className={`bg-gradient-to-r from-rose-400 to-fuchsia-500 text-transparent bg-clip-text ${styles.words}`} style={{ marginTop: `${marginTop}px` }}>
-                {words.split('').map((char, index) => {
-                    let color;
-                    let bgColor;
-                    if (index < charsTyped.length) {
-                        color = char === charsTyped[index] ? '#a5b4fc' : 'white';
-                        bgColor = char === charsTyped[index] ? 'transparent' : '#f87171';
 
-                    }
-                    return (
-                        <span key={index} 
-                              style={{ 
-                                backgroundColor: bgColor,
-                                color,
-                                borderRadius: '10px', 
-                                }}>
-                            {char}
-                            {index === charsTyped.length - 1 && <span ref={ref} className="cursor"></span>}
-                        </span>
-                    );
-                })}
-            </p>
+
         </div>
     );
 };
