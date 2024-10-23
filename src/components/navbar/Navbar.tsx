a010189599baf37e2717928f0c4ae2b4e17ffa2f
Keren Villatoro <kerenv@outlook.com>
2024-10-23 12:26:57 -0700

    build fix

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,6 @@ import styles from "./navbar.module.css";
 import Links from "./link/Links";
 import { LuFlower } from "react-icons/lu";
 import { LuUser } from "react-icons/lu";
-import { auth, signOut } from "@/auth";
 
 import {
     Avatar,
@@ -21,7 +20,6 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 const NavBar = async () => {
-    const session = await auth();
 
     return (
         <div className={styles.container}>
@@ -30,37 +28,6 @@ const NavBar = async () => {
             </div>
             <div className="flex justify-center items-center space-x-2">
                 <Links />
-
-                {session && session.user && (
-                    <DropdownMenu>
-                        <DropdownMenuTrigger>
-                            <Avatar className="w-8 h-8 outline-2 select-none">
-                                <AvatarImage src={session?.user.image ?? ""} />
-                                <AvatarFallback className="outline-2">
-                                    <LuUser className="w-28 h-28" />
-                                </AvatarFallback>
-                            </Avatar>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>
-                                <Link href="/account">Profile</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <form
-                                    className="flex justify-center"
-                                    action={async () => {
-                                        "use server";
-                                        await signOut();
-                                    }}
-                                >
-                                    <button type="submit">Sign Out</button>
-                                </form>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                )}
             </div>
         </div>
     );
