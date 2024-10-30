5306208ba00ed9624ebf7948fca671b757c4e613
Keren Villatoro <kerenv@outlook.com>
2024-10-29 19:31:36 -0700

    added demo and signup to home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 // Home.tsx
 import styles from "./home.module.css";
-import Hero from '@/components/hero/hero';
+import Hero from '@/components/homepage/hero/hero';
+import Demo from '@/components/homepage/demo/demo';
+import Signup from "@/components/homepage/signup/signup";
+
 export default function Home() {
   return (
         <main>
-          <Hero title={"bloomtype"} description={"Start your typing journey today and watch your skills flourish."} />
+          <Hero title={"bloom type"} description={"Start your typing journey today and watch your skills flourish."} />
+          <Demo />
+          <Signup />
         </main>
   );
 }
